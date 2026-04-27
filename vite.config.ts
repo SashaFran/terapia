c71564c5996b466bf297b76c2b253ece90eaed58
Sasha <sashafranchini@gmail.com>
2026-04-27 18:59:59 -0300

    vite

--- vite.config.ts
+++ vite.config.ts
@@ -1,15 +1,5 @@
 import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  plugins: [react()],
-  base: '/terapia/',
-  build: {
-    sourcemap: false,
-  },
-server: {
-  proxy: {
-    "/api": "http://localhost:3001"
-  }
-}
+  base: '/',
 })
